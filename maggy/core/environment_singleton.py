e3b6239912553ae83a5d8dd63cb6055150abd3c7
RiccardoGrigoletto <riccardogrigo@gmail.com>
2021-02-02 11:19:52 +0100

    modified environment check

--- maggy/core/environment_singleton.py
+++ maggy/core/environment_singleton.py
@@ -1,32 +1,25 @@
+import os
 
-def _is_hops_available():
-    try:
-        import hops
-    except ModuleNotFoundError:
-        return False
-    except ImportError:
-        return False
 
-    return True
 
 class EnvironmentSingleton:
     __instance = None
     def __new__(cls, *args):
         if cls.__instance is None:
             # check hopsworks availability
-            if _is_hops_available():
-
+            if "REST_ENDPOINT" in os.environ:
                 from maggy.core.environment import HopsEnvironment
                 cls.__instance = HopsEnvironment(cls, *args)
-                print("Hopsworks APIs are available.")
+
+            #todo: add condition for databricks
+            elif False:
+                from maggy.core.environment import DatabricksEnvironment
+                cls.__instance = DatabricksEnvironment(cls, *args)
             else:
                 from maggy.core.environment import BaseEnvironment
                 cls.__instance = BaseEnvironment(cls, *args)
-                print("Hopsoworks APIs are not available, using spark-only configuration.")
 
             if cls.__instance is None:
                 raise AttributeError("Environment is None.")
 
         return cls.__instance
-
-
